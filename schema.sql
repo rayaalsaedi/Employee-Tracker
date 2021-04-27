b114c33ac7ebc1244cba9181303909b7686e0e1e
Rulla <rulla@Rullas-MacBook-Pro-2.local>
2021-04-26 18:30:58 -0700

    adding js files

--- schema.sql
+++ schema.sql
@@ -6,24 +6,24 @@ use employeeDB;
 
 create table employee (
 id int not null auto_increment,
-roleId int,
-managerId int, 
-firstName varchar (50),
-lastName varchar (50),
+first_name varchar (30),
+last_name varchar (30),
+role_id int,
+manager_id int, 
 primary key (id)
 );
 
 create table department(
 id int not null auto_increment,
-departmentName varchar (60,
+name varchar (30),
 primary key (id)
 );
 
-create table positions (
+create table role (
 id int not null auto_increment,
-title varchar (50),
-pay int,
-departmentId int,
+title varchar (30),
+salary decimal(10,2),
+department_id int,
 primary key (id)
 );
 
